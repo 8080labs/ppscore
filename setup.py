959c2bed08735e1ea4c105563fc9659a1e63e21f
tkrabel <tobiaskrabel@gmx.de>
2020-08-21 10:33:19 +0200

    add installation of requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ import sys
 
 from pkg_resources import VersionConflict, require
 from setuptools import setup
+from pathlib import Path
 
 try:
     require("setuptools>=38.3")
@@ -19,5 +20,15 @@ except VersionConflict:
     sys.exit(1)
 
 
+def read_requirements(basename):
+    requirements_file = Path(__file__).parent.absolute() / basename
+    with open(requirements_file) as f:
+        return [req.strip() for req in f.readlines()]
+
+
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    requirements = read_requirements("requirements.txt")
+    setup(
+        use_pyscaffold=True,
+        install_requires=requirements
+    )
